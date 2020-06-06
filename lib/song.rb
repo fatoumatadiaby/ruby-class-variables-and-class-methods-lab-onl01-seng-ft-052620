578a96619858941a3b9b4508c795e816238d42c6
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-06 19:47:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,40 +1,39 @@
-class Song 
-   
-  attr_reader :name, :artist, :genre  
-  
+class Song
+  attr_reader :name, :artist, :genre
   @@count = 0
   @@artists = []
-  @@genres =[]
-  
- 
+  @@genres = []
   def initialize(name, artist, genre)
-   @name = name 
-   @artist = artist 
-   @genre = genre 
-   @@count += 1 
-   @@artists << artist
-   @@genres << genre 
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
   end
-
-  def self.count 
-  @@count 
- end 
- 
- def self.artists 
-  @@artists.uniq 
- end 
- 
- def self.genres 
-  @@genres.uniq 
- end 
- 
- def self.genre_count
-  
-   genre.hash = {}
-   
-   self.genres.each do |genre|
-     genre_hash[genre] = @@genres.count {|g| g == genre }
- end
-    genre_hash
- end 
-end 
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.genre_count
+    #@@genres -non unique array of genres
+    # self.genres - unique array of genres
+    genre_hash = {}
+    self.genres.each do |genre|
+      genre_hash[genre] = @@genres.count {|g| g == genre }
+  end
+  genre_hash
+  end
+  def self.artist_count
+    artist_hash = {}
+    self.artists.each do |artist|
+      artist_hash[artist] = @@artists.count {|a| a == artist }
+    end
+    artist_hash
+  end
+end
